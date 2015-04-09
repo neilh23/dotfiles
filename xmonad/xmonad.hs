535a9e280c164246d1be0999d47b76ea9333ce1e
Neil Howie <neil.howie@yakara.com>
2015-04-09 10:12:58 +0100

    Updates to xmonad settings

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -2,38 +2,60 @@ import XMonad
 import XMonad.Config.Gnome
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.Script
-import XMonad.Util.Run(spawnPipe)
+import XMonad.Layout.Fullscreen
+import XMonad.Layout.Grid
+import XMonad.Layout.IM
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Reflect
+import XMonad.Layout.Spacing
+import XMonad.Layout.Spiral
+import XMonad.Layout.Tabbed
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.Run(spawnPipe)
+import Data.Ratio ((%))
 import System.IO
 
 import qualified XMonad.StackSet as W
+
+myLayout = avoidStruts (
+    Tall 1 (3/100) (1/2) |||
+    Mirror (Tall 1 (3/100) (1/2)) |||
+    Full |||
+    spiral (6/7)) |||
+    noBorders (fullscreenFull Full)
  
 -- use 'xprop' to get the class information from a window
 myManageHook = composeAll
-    [ className =? "Gimp"      --> doFloat
-    , className =? "Gimp-2.6"      --> doFloat
-    , className =? "Ekiga"      --> doFloat
-    , className =? "Skype"      --> doFloat
-    , title =? "ALSA Mixer"	--> doFloat
+    [ className =? "Gimp"           --> doFloat
+    , className =? "Gimp-2.6"       --> doFloat
+    , className =? "Msgcompose"     --> doCenterFloat
+    , className =? "Ekiga"          --> doFloat
+    , className =? "Skype"          --> doFloat
+    , title     =? "Volume Control" --> doFloat
     , resource  =? "desktop_window" --> doIgnore
     , resource  =? "kdesktop"       --> doIgnore
     ]
 
+myWorkspaces = [ "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
+
 main = xmonad =<< xmobar myConfig
  
 myConfig = gnomeConfig {
-    manageHook = manageDocks <+> myManageHook -- make sure to include myManageHook definition from above
-                             <+> manageHook defaultConfig
-    , layoutHook = avoidStruts  $  layoutHook defaultConfig
+    manageHook    = manageDocks <+> myManageHook -- make sure to include myManageHook definition from above
+                                <+> manageHook defaultConfig
+    , borderWidth = 1
+    , layoutHook  = smartBorders $ myLayout
     , startupHook = execScriptHook "startup"
-    , modMask = mod4Mask     -- Rebind Mod to the Windows key
-    , terminal = "terminator"
+    , modMask     = mod4Mask     -- Rebind Mod to the Windows key
+    , terminal    = "terminator"
     } `additionalKeys`
-    [ ((mod4Mask, xK_a), spawn "alsamixergui")
+    [ ((mod4Mask, xK_a), spawn "pavucontrol")
     , ((mod4Mask, xK_p), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
     , ((mod4Mask, xK_v), spawn "gvim")
-    , ((mod4Mask, xK_f), spawn "nautilus")
+    , ((mod4Mask, xK_f), spawn "pcmanfm")
     , ((mod4Mask, xK_c), spawn "google-chrome")
+    , ((mod4Mask, xK_z), spawn "xscreensaver-command -lock")
     , ((mod1Mask, xK_Tab), windows W.focusDown)
     ]
